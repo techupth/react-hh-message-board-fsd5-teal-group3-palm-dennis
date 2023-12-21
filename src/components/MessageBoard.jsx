97b7f5cbf34763cd3d6486456cf77cd434b0dbd1
Palm <palml1lax@gmail.com>
2023-12-21 21:20:59 +0700

    feat: finish ex1

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,62 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [messageList, setMessageList] = useState([]);
+  const [message, setMessage] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const addMessage = () => {
+    setMessageList([...messageList, message]);
+    setMessage("");
+    setIsValid(false);
+  };
+  const deleteMessage = (i) => {
+    messageList.splice(i, 1);
+    setMessageList([...messageList]);
+  };
+  console.log(isValid);
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+              message ? setIsValid(true) : setIsValid(false);
+            }}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button
+          className="submit-message-button"
+          onClick={addMessage}
+          disabled={!isValid}
+        >
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {messageList.map((item, i) => {
+          return (
+            <div className="message">
+              <h1>{item}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  deleteMessage(i);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
